ef87cf1fcf9029b5f443f940716524eaa47e19f5
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-09-28 17:20:08 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -76,17 +76,6 @@ function Obstacles() {
   }
 }
 
-function setup() {
-  createCanvas(400, 400);
-  monkey = createSprite(200,380,20,50);
-  ground = createSprite(200,390,800,20);
-  banana =  createSprite(400,260,40,10);
-  obstacle = createSprite(400,365,10,40);
-}
 
-function preload(){
-  monkey = loadAnimation("monkey_01.png","monkey_02.png","monkey_03.png","monkey_04.png","monkey_05.png","monkey_06.png","monkey_07.png","monkey_08.png","monkey_09.png","monkey_10.png",);
-  obstacle = loadIamge("stone.png");
-  backImage=loadImage("jungle2.jpg");
 }
 
