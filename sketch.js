316b135ce1d908282c44aa59422642cf20c902de
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-11-29 20:01:29 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,81 +1,49 @@
 var monkey
-var ground 
-var jungle2Image
-//scale and position the monkey
-monkey.scale = 0.1;
-monkey.x = 50;
-monkey.y = 350;
+var invisibleGround;
+var obstaclesGroup, Stone;
+var background1
+ 
 
-var bananaGroup;
-var ObstaclesGroup;
 
 function preload(){
-  monkey = loadAnimation("monkey_01.png","monkey_02.png","monkey_03.png","monkey_04.png","monkey_05.png","monkey_06.png","monkey_07.png","monkey_08.png","monkey_09.png","monkey_10.png",);
-  obstacle = loadIamge("stone.png");
-  backImage=loadImage("jungle2.jpg");
-  backgr=createSprite(0,0,800,400);
-  backgr.addImage(jungle2);
+  monkey = loadImage("monkey_01,");
+  
+    background1 = loadImage*("Jungle.jpg")
+  obstacleGroup = loadImage("stone.png");
 }
 
 function setup() {
-  createCanvas(400, 400);
-  monkey = createSprite(200,380,20,50);
-  ground = createSprite(200,390,800,20);
-  bananaGroup =  createSprite(400,260,40,10);
-  obstacleGroup = createSprite(400,365,10,40);
+  createCanvas(600, 200);
+  
+  monkey = createSprite(50,180,20,50);
+  monkey.scale = 0.5;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  obstaclesGroup = new Group();
 }
-
 function draw() {
   background(255);
-  
-  ground.velocityX = -3;
-  
-  if (ground.x < 0){
-      ground.x = ground.width/2;
-    }
-  
- if(keyDown("space")){
-      monkey.velocityY = -12 ;
-    }
-
-//add gravity
-    monkey.velocityY = monkey.velocityY + 0.8;
-  
-  monkey.collide(ground);
-  Obstacles();
-  drawSprites();
-  Banana();
-}
-function Banana (){
-  //code to spawn bananas
-  if (World.frameCount % 200 === 0) {
-    var banana 
-    //banana.scale = 0.05;
-    banana.velocityX = -3;
     
-    banana.lifetime = 134;
-    
-    banana.depth = monkey.depth;
-    monkey.depth = monkey.depth + 1;
-    
-    //add each banana to the group
-    bananaGroup.add(banana);
+ if(keyDown("space") && monkey.y >= 150){
+    monkey.velocityY = -10;
   }
-}
+  
+  monkey.velocityY = monkey.velocityY + 0.8
+     monkey.collide(invisibleGround);
+  spawnObstacles();
+  drawSprites()
+  }
 
-function Obstacles() {
-  if(World.frameCount % 120 === 0) {
-    var obstacle 
-    obstacle.velocityX = -3;
-    
+function spawnObstacles() {
+  if(frameCount % 60 === 0) {
+    var obstacle = createSprite(600,165,10,40);
+    obstacle.velocityX = -4;
     //assign scale and lifetime to the obstacle           
-    //obstacle.scale = 0.2;
-    obstacle.lifetime = 134;
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
     //add each obstacle to the group
-    ObstaclesGroup.add(obstacle);  
+    obstaclesGroup.add(obstacle);
   }
 }
-
-
-}
-
