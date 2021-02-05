201bf836c34d0b2f9bc1b52db0b3d17de6f09db4
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2021-02-04 20:25:02 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,52 +1,73 @@
-var monkey
-var invisibleGround;
-var obstaclesGroup, Stone;
-var background1
- 
+/*var monkey
+var ground 
+//scale and position the monkey
+monkey.scale = 0.1;
+monkey.x = 50;
+monkey.y = 350;
 
+var bananaGroup;
+var ObstaclesGroup;
+*/
 
-function preload(){
-  monkey = loadImage("monkey_01,");
-  
-    background1 = loadImage*("Jungle.jpg")
-  obstacleGroup = loadImage("stone.png");
-}
-
-function setup() {
-  createCanvas(600, 200);
-  
-  monkey = createSprite(50,180,20,50);
-  monkey.scale = 0.5;
-  
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
-  
-  obstaclesGroup = new Group();
-}
 function draw() {
   background(255);
-    
- if(keyDown("space") && monkey.y >= 150){
-    monkey.velocityY = -10;
-  }
+  /*
+  ground.velocityX = -3;
   
-  monkey.velocityY = monkey.velocityY + 0.8
-     monkey.collide(invisibleGround);
-monkey.display()
- invisibleGround.display()
- 
-  spawnObstacles();
-  drawSprites()
-  }
+  if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+  
+ if(keyDown("space") && monkey >= 359){
+      monkey.velocityY = -12 ;
+    }
 
-function spawnObstacles() {
-  if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,165,10,40);
-    obstacle.velocityX = -4;
-    //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.5;
-    obstacle.lifetime = 300;
-    //add each obstacle to the group
-    obstaclesGroup.add(obstacle);
+//add gravity
+    monkey.velocityY = monkey.velocityY + 0.8;
+  
+  monkey.collide(ground);
+  Obstacles();
+  drawSprites();
+  Banana();
+*/
   }
+function Banana (){
+  //code to spawn bananas
+  if (World.frameCount % 200 === 0) {
+    var banana 
+    //banana.scale = 0.05;
+    banana.velocityX = -3;
+    
+    banana.lifetime = 134;
+    
+    banana.depth = monkey.depth;
+    monkey.depth = monkey.depth + 1;
+    
+    //add each banana to the group
+    bananaGroup.add(banana);
+  }
+}
+
+function Obstacles() {
+  if(World.frameCount % 120 === 0) {
+    var obstacle 
+    obstacle.velocityX = -3;
+    
+    //assign scale and lifetime to the obstacle           
+    //obstacle.scale = 0.2;
+    obstacle.lifetime = 134;
+    //add each obstacle to the group
+    ObstaclesGroup.add(obstacle);  
+  }
+}
+
+function preload(){
+
+}
+function setup() {
+  createCanvas(400, 400);
+  monkey = createSprite(200,380,20,50);
+  ground = createSprite(200,390,800,20);
+  banana =  createSprite(400,260,40,10);
+  obstacle = createSprite(400,365,10,40);
 }
